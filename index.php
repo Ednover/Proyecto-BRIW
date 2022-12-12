0a30182008d5996288178f1d20fa247dc4877507
Ednover <ed_leo1411@hotmail.com>
2022-12-12 16:31:24 -0600

    actualizacion de consulta
    se consulta la informacion de los resultados, simples, sin ranking y en otra pagina

--- index.php
+++ index.php
@@ -20,71 +20,28 @@
         <div class="documents"></div>
         </div>
     </body>
-    <script>
-        const fetchSearch = async (search) => {
-        let data;
-        try {
-            //si no hay palabra presente todo muere
-            if(!search){
-                setSpell([]);
-                setExpandWords([]);
-                setSearchResult([]);
-                return;
-            }
-            //consulta la corrección de palabras
-            let spell_words = await fetch('http://localhost:8983/solr/briwtest/spell?q=' + search);
-            let _spell = await spell_words.json();
-            console.log(_spell);
-            //verifica la existencia de sugerencias y actualiza el estado
-            if(_spell.spellcheck){
-                setSpell(_spell.spellcheck.suggestions.length ? _spell.spellcheck.suggestions[1].suggestion : []);
-            }else{
-                setSpell([]);
-            }
-            //expande las busqueda 
-            let expand_words = await fetch('https://api.datamuse.com/words?ml=' + search + '&v=es&max=10');
-            let words = await expand_words.json()
-            setExpandWords(words ? words : [])
-            let expand_search = search;
-            for (let index = 0; index < words.length; index++) {
-                expand_search = expand_search + " OR " + words[index].word;
-
-            }
-            console.log(expand_search ? expand_search : []);
-
-            //realiza la busqueda de las palabras expandidas
-            data = await fetch('http://localhost:8983/solr/briwtest/select?rows=100&fl=*%2Cscore&q=' + expand_search);
-            const items = await data.json();
-            console.log(items.response.docs);
-            setSearchResult(items.response.docs);
-        } catch (err) {
-            console.log(err);
-        }
-
-    };
-
-    const searchChangeHandler = (event) => {
-        setSearch(event.target.value);
-    }
-    
-    useEffect(() => {
-        async function fetchData() {
-            try {
-            let data = await fetch('http://localhost:8983/solr/briwtest/suggest?suggest=true&suggest.build=true&suggest.dictionary=default&wt=json&suggest.q=' + search);
-            const result = await data.json();
-            console.log(search);
-            console.log(result.suggest.default[search].suggestions);
-            setSuggests(result.suggest.default[search].suggestions)
-        } catch (err) {
-            console.log(err);
-        }
-        }
-        fetchData();
-    }, [search]);
-
-    const SubmitHandler = (event) => {
-        event.preventDefault();
-        fetchSearch(search)
-    }
-    </script>
 </html>
+
+
+<?php
+/* if (isset($_POST['SubmitFile'])){
+    $search = $_POST['search'];
+
+    $query = file_get_contents('http://localhost:8983/solr/briwtest/spell?q=' . $search);
+
+    $array = json_decode($query, true);
+
+    $results = $array['response']['docs'];
+
+    foreach($results as $key => $doc){
+        echo '<br>';
+        echo $doc['title'][0];
+        echo '<br>';
+        echo $doc['description'][0];
+        echo '<br>';
+        echo $doc['url'][0];
+    }
+
+    echo "<script>alert('Se actualizaron las URLs')</script>";
+} */
+?>
--- querySolr.php
+++ querySolr.php
@@ -2,8 +2,18 @@
 
 $search = $_POST['search'];
 
-$query = file_get_contents('http://localhost:8983/solr/briwtest/spell?q=' . $search);
+    $query = file_get_contents('http://localhost:8983/solr/briwtest/spell?q=' . $search);
 
-echo $query;
+    $array = json_decode($query, true);
 
+    $results = $array['response']['docs'];
+
+    foreach($results as $key => $doc){
+        echo '<br>';
+        echo $doc['title'][0];
+        echo '<br>';
+        echo $doc['description'][0];
+        echo '<br>';
+        echo $doc['url'][0];
+    }
 ?>
