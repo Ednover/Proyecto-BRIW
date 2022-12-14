50208389584719e6a4053eec5546b8903a52cfc5
Ednover <ed_leo1411@hotmail.com>
2022-12-14 16:23:20 -0600

    error de espacios

--- mostrar.php
+++ mostrar.php
@@ -1,9 +1,11 @@
 <?php
 $searchTerm = $_POST['palabra'];
-$url = 'http://localhost:8983/solr/briwsolr/suggest?suggest=true&suggest.build=true&suggest.dictionary=default&wt=json&suggest.q=' . $searchTerm;
+$searchTerm = str_replace(" ", '+', $searchTerm);
+$array = explode("+", $searchTerm);
+$url = 'http://localhost:8983/solr/briwsolr/suggest?suggest=true&suggest.build=true&suggest.dictionary=default&wt=json&suggest.q=' . $array[0];
 $query = file_get_contents($url);
 $responseSug = json_decode($query, true);
-$suggestions = $num = $responseSug['suggest']['default'][$searchTerm]["suggestions"];
+$suggestions = $num = $responseSug['suggest']['default'][$array[0]]["suggestions"];
 
 foreach($suggestions as $key => $value){
 	echo $value['term']."<br>";
